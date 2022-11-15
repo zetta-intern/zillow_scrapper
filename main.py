a57a467390e8620a8d2d5a7bd20088831295f1eb
Muchamad Faiz <muchamadfaiz@gmail.com>
2022-11-15 09:42:37 +0700

    first function

---
+++ main.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+
+def get_page_link():
+    pass
+
+def extract_info():
+    pass
+
+def save_CSV():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
